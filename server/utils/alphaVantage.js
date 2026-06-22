4c7f379b126c900efa2c028f64ebea639e44e984
Samik <samik.sphs@gmail.com>
2026-06-22 22:38:53 +0530

    Update alphaVantage.js

--- server/utils/alphaVantage.js
+++ server/utils/alphaVantage.js
@@ -1,10 +1,47 @@
 const axios = require('axios');
 
-const fetchMarketData = async (symbol) => {
-  const res = await axios.get(
-    `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${symbol}&apikey=${process.env.ALPHA_VANTAGE_KEY}`
-  );
-  return res.data;
+const alphaVantageClient = axios.create({
+  baseURL: 'https://www.alphavantage.co/query',
+  timeout: parseInt(process.env.API_TIMEOUT) || 15000,
+});
+
+const getStockQuote = async (symbol) => {
+  const response = await alphaVantageClient.get('', {
+    params: {
+      function: 'GLOBAL_QUOTE',
+      symbol: symbol,
+      apikey: process.env.ALPHA_VANTAGE_API_KEY,
+    },
+  });
+  return response.data;
 };
 
-module.exports = fetchMarketData;
+const getStockDailyTimeSeries = async (symbol, outputsize = 'compact') => {
+  const response = await alphaVantageClient.get('', {
+    params: {
+      function: 'TIME_SERIES_DAILY',
+      symbol: symbol,
+      outputsize: outputsize,
+      apikey: process.env.ALPHA_VANTAGE_API_KEY,
+    },
+  });
+  return response.data;
+};
+
+const searchStockSymbol = async (keywords) => {
+  const response = await alphaVantageClient.get('', {
+    params: {
+      function: 'SYMBOL_SEARCH',
+      keywords: keywords,
+      apikey: process.env.ALPHA_VANTAGE_API_KEY,
+    },
+  });
+  return response.data;
+};
+
+module.exports = {
+  alphaVantageClient,
+  getStockQuote,
+  getStockDailyTimeSeries,
+  searchStockSymbol,
+};
